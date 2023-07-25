cfadc0d1435c5a88a85e2ca71995ce59e4bebaeb
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-07-25 01:01:50 -0600

    Seccion 4 completada

---
+++ seccion1/04-Numeros/js/01-app.js
@@ -0,0 +1,14 @@
+//Creacion de números
+const numero = 34;
+console.log(numero);  
+const numero2 = -34;
+console.log(numero2);  
+const numero3 = 0.5;
+console.log(numero3);  
+const numero4 = 0.6984134;
+console.log(numero4);  
+const numero5 = 1.7;
+console.log(numero5);
+
+const numero6 = new Number(10);
+console.log(numero6);
---
+++ seccion1/04-Numeros/js/04-app.js
@@ -0,0 +1,16 @@
+let resultado;
+
+//Jerarquia de operaciones
+resultado = 20 + 30 * 2;
+console.log(resultado);
+
+resultado = (20 + 30) * 2;
+console.log(resultado);
+
+//20% de descuento en un carrito de compras
+resultado = (10 + 20 + 40 + 90) * 0.2;
+console.log(resultado);
+
+//IVA
+resultado = (768) * 1.16;
+console.log(resultado);

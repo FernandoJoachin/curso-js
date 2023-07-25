5f98afd48d95049a1aec27483a08062dbab16383
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-07-24 23:15:54 -0600

    Seccion 2 completada

---
+++ seccion1/02-Variables/js/01-app.js
@@ -0,0 +1,27 @@
+//Instanciar una variable sin asignarle un valor
+var precio;
+precio = 34;
+console.log(precio);
+
+//Inicializar una variable
+var producto = "Televisor HD";
+console.log(producto);
+
+//Inicializar múltiples variables
+var cancion = "Nota",
+    artista = "Nicki Nicole",
+    duracion = "2:39";
+console.log(artista);
+
+//Las varaibles se pueden reasignar
+producto = "Computadora Gamer";
+console.log(producto);
+
+//Javascript es un lenguaje de programación de tipo dinámico, en donde no se especifica el tipo de dato (no es tipado)
+producto = 50;
+console.log(producto);
+
+//Nombrado de variables
+var nombreProducto;
+var nombre_producto;
+var NombreProducto;

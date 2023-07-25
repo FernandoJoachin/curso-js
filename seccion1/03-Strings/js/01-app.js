f4b867d5db80bb6d2247d88dbe42739b19b2a484
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-07-25 00:21:11 -0600

    Seccion 3 completada

---
+++ seccion1/03-Strings/js/01-app.js
@@ -0,0 +1,6 @@
+const producto = "Monitor de 24 \"";
+console.log(producto);
+const producto2 = String('Monitor de 24"');
+console.log(producto2);
+const producto3 = new String("Monitor de 24 pulgadas");
+console.log(producto3);
---
+++ seccion1/03-Strings/js/04-app.js
@@ -0,0 +1,9 @@
+const producto = "       Monitor curvo          ";
+console.log(producto);
+console.log(producto.length);
+
+//Eliminar espacios en blanco
+console.log(producto.trimStart());
+console.log(producto.trimEnd());
+console.log(producto.trimStart().trimEnd());
+console.log(producto.trim());

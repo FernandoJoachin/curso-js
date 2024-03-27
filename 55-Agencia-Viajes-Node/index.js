849fc59b33bc936cd6a67befd293c5f9faa7a778
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-03-27 03:21:40 -0600

    Avance de la Seccion 55

---
+++ 55-Agencia-Viajes-Node/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import router from "./routes/index.js";
+import db from "./config/db.js";
+
+const app = express();
+
+//Conectar a la DB
+db.authenticate()
+    .then(() => console.log('Base de datos conectada'))
+    .catch(error => console.log(error))
+
+//Definir puerto
+const port = process.env.PORT || 4000;
+
+//Habilitar PUG
+app.set('view engine', 'pug');
+
+//Definiar la carpeta publica
+app.use(express.static('public'));
+
+//Obtener el año actual
+app.use((req, res, next) => { //req - lo que enviamos, res - lo que express nos responde, next para ir al siguiente
+    const date = new Date();
+    res.locals.year = date.getFullYear();
+    res.locals.nombreSitio = 'Agencia de viajes'
+    return next()
+});
+
+//Agregar router
+app.use('/', router);
+
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
